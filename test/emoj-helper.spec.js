0a2ab35f1f06b5a0c2d714a7ca94e3d3cdb50e78
Simon Wicki <simon.wicki@justwatch.com>
2016-06-28 00:22:46 +0200

    feat: added helper

---
+++ test/emoj-helper.spec.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const assert = require('chai').assert;
+const emojiHelper = require('../src/emoji-helper');
+
+describe('emoji helper', () => {
+
+    it('should determine if a text starts with a URL', () => {
+        const checks = [
+            {text: 'just some text', result: false},
+            {text: 'https://www.disney.com', result: true},
+            {text: 'https://www.disney.com with some appending text', result: false},
+            {text: 'with prepending text https://www.disney.com', result: false},
+        ];
+
+        checks.forEach(item => {
+            const result = emojiHelper.isUrl(item.text)
+            assert.equal(result, item.result);
+        });
+    });
+
+});
